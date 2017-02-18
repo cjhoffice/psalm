216494dfeb3fc2f5029bb9d0034804bbaa9f80a5
Matthew Brown <github@muglug.com>
2017-02-17 19:52:07 -0500

    Add classes from felixfbecker/php-language-server

--- src/Psalm/IssueBuffer.php
+++ src/Psalm/IssueBuffer.php
@@ -201,6 +201,9 @@ class IssueBuffer
             "\e[0m" . substr($snippet, $selection_length + $selection_start) . PHP_EOL;
     }
 
+    /**
+     * @return array
+     */
     public static function clear()
     {
         $current_data = self::$issue_data;
